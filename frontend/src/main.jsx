1a4685819fe8d9e2191e09f4a3fb968baf6b01b3
Naveen pandian <npandian515@gmail.com>
2026-01-31 23:44:00 +0530

    Add transport configuration for Sepolia chain in RainbowKit setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,18 @@ import {getDefaultConfig, RainbowKitProvider,darkTheme} from '@rainbow-me/rainbo
 import './index.css'
 import App from './App.jsx'
 import { sepolia } from 'viem/chains'
+import { http } from 'viem'
 
 const projectID = import.meta.env.VITE_PROJECT_ID;
 
 const config = getDefaultConfig({
   appName: 'Speak-Defi',
   projectId: projectID,
-  chains:[sepolia]
+  chains:[sepolia],
+  transports: {
+    [sepolia.id]: http("")
+  }
+
 })
 
 createRoot(document.getElementById('root')).render(

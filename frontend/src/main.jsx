2a9b01c10e0479fb19b5f389d114a86d154fd6b7
Naveen pandian <npandian515@gmail.com>
2026-01-31 23:42:42 +0530

    Add configuration for RainbowKit with project ID and chains

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {getDefaultConfig, RainbowKitProvider,darkTheme} from '@rainbow-me/rainbowkit'
 import './index.css'
 import App from './App.jsx'
+import { sepolia } from 'viem/chains'
 
 const projectID = import.meta.env.VITE_PROJECT_ID;
+
+const config = getDefaultConfig({
+  appName: 'Speak-Defi',
+  projectId: projectID,
+  chains:[sepolia]
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

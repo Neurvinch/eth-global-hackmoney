8e9a66ff5a39171f6c993c6f0835d5f1f3e401e6
Naveen pandian <npandian515@gmail.com>
2026-02-01 23:17:34 +0530

    adjust config

--- backend/services/nlpServices.js
+++ backend/services/nlpServices.js
@@ -1,3 +1,17 @@
 require('dotenv/config');
 
-const {} = require("")
+const {Groq} = require("groq-sdk")
+const fs = require('fs');
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY,
+});
+
+
+const transcribeAudio = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
